efd88b5afbc1c1d63a7840ee185c5e9074bf1fee
kynefuk <atsuminamihira@gmail.com>
2020-11-28 22:19:05 +0900

    fix

--- server/myapp/settings.py
+++ server/myapp/settings.py
@@ -4,7 +4,10 @@ from pydantic import BaseSettings, AnyHttpUrl
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ['http://localhost:3000']
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
+        "http://localhost:3000",
+        "http://34.107.239.159",
+    ]
 
 
 settings = Settings(PROJECT_NAME="hoge")
